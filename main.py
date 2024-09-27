54b02e36ff6a6c3994d7a0d39aa44302ed9d3848
AliceWilliamson <alicewilliamson13@gmail.com>
2024-09-26 22:52:14 -0600

    drew player, and added movement controls

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-import pygame
+import pygame # type: ignore
 from constants import *
+from player import *
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+
         screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
         
 
 
