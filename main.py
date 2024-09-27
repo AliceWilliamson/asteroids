db969b9d0f0a5fc8f25bf0f08f1adbf63d8949de
AliceWilliamson <164314293+AliceWilliamson@users.noreply.github.com>
2024-09-26 20:37:53 -0600

    limited game to 60fps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -10,6 +12,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 
 
