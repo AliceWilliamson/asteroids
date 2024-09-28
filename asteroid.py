439fbe1ea5baffdd1230e71bd143bc52804a0446
AliceWilliamson <alicewilliamson13@gmail.com>
2024-09-27 20:24:31 -0600

    working on drawing asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,12 +1,11 @@
-from circleshape import *
+import pygame
+from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
+        super().__init__(x, y, radius)
 
-    def draw(self, x, y, radius):
-
+    def draw(self, screen):
+        pygame.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.radius, 2)
 
 pass
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame # type: ignore
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -9,7 +11,10 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
     
     
