f504cec1cc4f0afdf98af3ca264ad94d8021fa87
AliceWilliamson <164314293+AliceWilliamson@users.noreply.github.com>
2024-09-27 19:26:23 -0600

    added Player to groups, started on asteroid logic

---
+++ asteroid.py
@@ -0,0 +1,12 @@
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(self, x, y, radius):
+
+
+pass
